652fa195cc27d773341760733ca4f4a8ca2fd60a
Giacomo Beretta <giacopot@gmail.com>
2026-04-07 18:10:41 +0200

    first vector implementation

--- TracerLib/Vector.cs
+++ TracerLib/Vector.cs
@@ -2,5 +2,14 @@ namespace TracerLib;
 
 public struct Vector
 {
+    public float X { get; }
+    public float Y { get; }
+    public float Z { get; }
     
+    public Vector(float x, float y, float z)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+    }
 }
