c62af65ba3bed5b090ed08dfd6675cb965547edd
Simone Selmi <sselmi27@gmail.com>
2026-04-15 11:35:21 +0200

    Add Point and Normal Classes with Tests

--- TracerLib/Normal.cs
+++ TracerLib/Normal.cs
@@ -4,5 +4,76 @@ namespace TracerLib;
 
 public class Normal
 {
+    public float X { get; }
+    public float Y { get; }
+    public float Z { get; }
+
+    public Normal(float x, float y, float z)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+    }
+
+    public static bool _AreCloseNormal(Normal a, Normal b)
+    {
+        return Functions.AreClose(a.X, b.X) && Functions.AreClose(a.Y, b.Y) && Functions.AreClose(a.Z, b.Z);
+    }
+
+    public Normal _NormalNegation()
+    {
+        return new Normal(-this.X, -this.Y, -this.Z);
+    }
+
+    public static Normal operator *(Normal n, float a)
+    {
+        return new Normal(a * n.X, a * n.Y, a * n.Z);
+    }
+    
+    public static Normal operator *(float a, Normal n)
+    {
+        return n * a;
+    }
+
+    public static float operator *(Normal n, Vector v)
+    {
+        return n.X * v.X + n.Y * v.Y + n.Z * v.Z;
+    }
+    
+    public static float operator *(Vector v, Normal n)
+    {
+        return n * v;
+    }
+
+    public static Vector CrossProduct(Vector v, Normal n)
+    {
+        return new Vector(v.Y*n.Z - v.Z*n.Y, v.Z*n.X - v.X*n.Z, v.X*n.Y - v.Y*n.X);
+    }
+
+    public static Vector CrossProduct(Normal n, Vector v)
+    {
+        return new Vector(n.Y*v.Z - n.Z*v.Y, n.Z*v.X - n.X*v.Z, n.X*v.Y - n.Y*v.X);
+    }
+
+    public static Vector CrossProduct(Normal n, Normal v)
+    {
+        return new Vector(n.Y*v.Z - n.Z*v.Y, n.Z*v.X - n.X*v.Z, n.X*v.Y - n.Y*v.X);
+    }
+
+    public float SquaredNorm()
+    {
+        return MathF.Pow(this.X, 2) + MathF.Pow(this.Y, 2) + MathF.Pow(this.Z, 2);
+    }
+
+    public float Norm()
+    {
+        return MathF.Sqrt(this.SquaredNorm());
+    }
+
+    public Normal Normalize()
+    {
+        return new Normal(this.X, this.Y, this.Z) * (1 / this.Norm());
+    }
+    
     
 }
--- TracerLib/Point.cs
+++ TracerLib/Point.cs
@@ -4,5 +4,51 @@ namespace TracerLib;
 
 public struct Point
 {
-    
+    public float X { get; }
+    public float Y { get; }
+    public float Z { get; }
+
+    public Point(float x, float y, float z)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+    }
+
+    public override string ToString()
+    {
+        var point = "Point(x=" + X + ", y=" + Y +", z=" + Z + ")";
+        return point;
+    }
+
+    public static bool _AreClosePoint(Point a, Point b)
+    {
+        return Functions.AreClose(a.X, b.X) && Functions.AreClose(a.Y, b.Y) && Functions.AreClose(a.Z, b.Z);
+    }
+
+    public static Point operator +(in Point a, in Vector b)
+    {
+        return new Point(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+    }
+
+    public static Point operator +(in Vector a, in Point b)
+    {
+        return b + a;
+    }
+
+    public static Vector operator -(in Point a, in Point b)
+    {
+        return new Vector(a.X - b.X, a.Y - b.Y, a.Z - b.Z);
+    }
+
+    public static Point operator -(in Point a, in Vector b)
+    {
+        return new Point(a.X - b.X, a.Y - b.Y, a.Z - b.Z);
+    }
+
+    public Vector ToVector()
+    {
+        return new Vector(this.X, this.Y, this.Z);
+    }
+
 }
