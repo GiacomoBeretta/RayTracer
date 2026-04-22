350d21d98e3a0f104bba929d6160aaa16f6f396f
Giacomo Beretta <giacopot@gmail.com>
2026-04-22 17:42:22 +0200

    Fix missing braces

--- TracerLib/Camera.cs
+++ TracerLib/Camera.cs
@@ -9,19 +9,21 @@ public struct OrthogonalCamera : ICamera
 {
     public float AspectRatio { get; set; }
     public Transformation Transformation { get; set; }
-    
+
     public OrthogonalCamera(float aspectRatio, Transformation transformation)
     {
         AspectRatio = aspectRatio;
         Transformation = transformation;
     }
+
     public Ray FireRay(float u, float v)
     {
         var origin = new Point(-1f, (1f - 2f * u) * this.AspectRatio, 2f * v - 1f);
         return new Ray(); //Da modificare 
-    } 
-    
-    public struct PerspectiveCamera: ICamera
+    }
+}
+
+public struct PerspectiveCamera: ICamera
     {
         public float Distance { get; set; }
         public float AspectRatio { get; set; }
