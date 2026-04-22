623f05cfb74fad2cf906806d2ce1f8885b02b631
Giacomo Beretta <giacopot@gmail.com>
2026-04-22 17:32:29 +0200

    Add some docstrings

--- TracerLib/ImageTracer.cs
+++ TracerLib/ImageTracer.cs
@@ -1,17 +1,22 @@
 // This file is release under EUPL_v1.2 license. See LICENSE.md
 
+//da riscrivere meglio la docstring della classe una volta capito meglio cosa fa e magari rivedere le altre docstring
+
 using SixLabors.ImageSharp.Processing;
 
 namespace TracerLib;
 
+/// <summary>
+/// ImageTracer is a class
+/// </summary>
 public class ImageTracer
 {
     private HDRImage image;
     private ICamera camera;
 
     /// <summary>
-    ///
-    /// uPixel and vPixel are the coordinates inside the pixel at which the ray will be fired.
+    /// Returns the Ray that passes through the pixel at (column, row).
+    /// Since a pixel is not a point uPixel and vPixel are the coordinates inside the pixel at which the ray will be fired.
     /// If (uPixel, vPixel)=(0,0) it means the ray will be fired at the left bottom angle of the pixel
     /// </summary>
     /// <param name="column"></param>
@@ -26,6 +31,10 @@ public class ImageTracer
         return camera.FireRay(u, v);
     }
 
+    /// <summary>
+    /// Fires a ray for each pixel and use the <c>Renderer</c> function to solve the rendering equation and compute the color.
+    /// </summary>
+    /// <param name="Renderer"></param>
     public void FireAllRays(Func<Ray, Color> Renderer)
     {
         for (int col = 0; col < image.Width; col++)
