3b77ae403e66da2a724029e944c87c01e3b3b948
Giacomo Beretta <giacopot@gmail.com>
2026-04-25 20:59:05 +0200

    Change multiplication order, now it is Transformation * Ray, before it was the inverse

--- TracerLib/Ray.cs
+++ TracerLib/Ray.cs
@@ -4,8 +4,8 @@ public struct Ray
 {
     public Point Origin { get; set; }
     public Vector Dir { get; set; }
-    public float Tmin { get; private set; } 
-    public float Tmax { get; private set; } 
+    public float Tmin { get; private set; }
+    public float Tmax { get; private set; }
     public int Depth { get; private set; }
 
     public Ray(Point origin, Vector dir, float tmin = 1e-5f, float tmax = float.PositiveInfinity, int depth = 0)
@@ -19,17 +19,16 @@ public struct Ray
 
     public static bool _AreRaysClose(Ray r1, Ray r2)
     {
-        return Vector._AreVectorsClose(r1.Dir, r2.Dir) && Point._ArePointsClose(r1.Origin, r2.Origin);  
+        return Vector._AreVectorsClose(r1.Dir, r2.Dir) && Point._ArePointsClose(r1.Origin, r2.Origin);
     }
 
     public Point At(float t)
     {
-        return this.Origin + this.Dir * t; 
+        return this.Origin + this.Dir * t;
     }
 
-    public static Ray operator *(Ray r, Transformation t)
+    public static Ray operator *(Transformation t, Ray r)
     {
         return new Ray(t * r.Origin, t * r.Dir);
     }
-    
 }
