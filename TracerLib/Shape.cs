37c36bfa64d3a4685b2dcea3a38b72af9a8211ec
Giacomo Beretta <giacopot@gmail.com>
2026-04-26 03:32:08 +0200

    Implement sphere with tests (ray intersection test is still unfinished)

--- TracerLib/Shape.cs
+++ TracerLib/Shape.cs
@@ -2,15 +2,100 @@
 
 namespace TracerLib;
 
+/// <summary>
+/// An abstract class to represent all the possible 3D geometric shapes that will compose the scene
+/// </summary>
 public abstract class Shape
 {
+    /// <summary>
+    /// Returns a <c>HitRecord</c> object if there is an intersection between the <c>Ray</c> passed as argument
+    /// and *this* shape, otherwise returns a null value.
+    /// See <c>HitRecord</c> for more information.
+    /// </summary>
+    /// <param name="ray"></param>
+    /// <returns></returns>
     public abstract HitRecord? RayIntersection(Ray ray);
 }
 
+/// <summary>
+/// A 3D unit Sphere.
+/// The transform field allows to represent also translated and rotated ellipsoids.
+/// </summary>
 public class Sphere : Shape
 {
+    public Transformation Transform { get; }
+
+    public Sphere()
+    {
+        Transform = new Transformation();
+    }
+
+    public Sphere(Transformation transform)
+    {
+        this.Transform = transform;
+    }
+
+    /// <summary>
+    /// Returns the normal to the Sphere surface
+    /// depending on the direction dir of the Ray incident on the <c>Point</c> p.
+    /// </summary>
+    /// <param name="p"></param>
+    /// <param name="dir"></param>
+    /// <returns></returns>
+    public Normal _SphereNormal(Point p, Vector dir)
+    {
+        Normal normal = new Normal(p.X, p.Y, p.Z);
+        return normal * dir < 0 ? normal : -normal;
+    }
+
+    /// <summary>
+    /// Returns the longitude and colatitude normalized of the <c>Point</c> p.
+    /// </summary>
+    /// <param name="p"></param>
+    /// <returns></returns>
+    public Vector2D _SpherePointToUV(Point p)
+    {
+        return new Vector2D(MathF.Atan2(p.Y, p.X) / (2 * MathF.PI), MathF.Acos(p.Z) / MathF.PI);
+    }
+    
     public override HitRecord? RayIntersection(Ray ray)
     {
-        
+        // instead of transforming the sphere to represent all sorts of ellipsoids
+        // we transform the ray with the inverse transformation
+        Ray T_ray = (Transform.Inverse()) * ray;
+        Vector dir = T_ray.Dir;
+        Vector origin = T_ray.Origin.ToVector();
+        float delta_4 = (origin * dir) * (origin * dir) - dir.SquaredNorm() * (origin.SquaredNorm() - 1);
+        // if delta_4 < 0 there are no intersection, if delta_4 == 0 there is no reflection
+        // then we take only one of the two solutions depending on whether they represent
+        // intersections on the line that are behind the origin of the ray.
+        // See Ray struct for more information on tmin and tmax
+        if (delta_4 > 0)
+        {
+            float sqrt_delta_4 = MathF.Sqrt(delta_4);
+            float t1 = -(origin * dir + sqrt_delta_4) / dir.SquaredNorm();
+            if (t1 > T_ray.Tmin && t1 < T_ray.Tmax)
+            {
+                Point p = ray.At(t1);
+                return new HitRecord(p,
+                    Transform * _SphereNormal(p, dir),
+                    _SpherePointToUV(p),
+                    ray,
+                    t1);
+            }
+
+            float t2 = (-(origin * dir) + sqrt_delta_4) / dir.SquaredNorm();
+            if (t2 > T_ray.Tmin && t2 < T_ray.Tmax)
+            {
+                Point p = ray.At(t2);
+                return new HitRecord(p,
+                    Transform * _SphereNormal(p, dir),
+                    _SpherePointToUV(p),
+                    ray,
+                    t2);
+            }
+        }
+
+        return null; //no intersection
     }
 }
---
+++ TracerTests/ShapeTest.cs
@@ -0,0 +1,66 @@
+using TracerLib;
+
+namespace TracerTests;
+
+public class SphereTest
+{
+    [Fact]
+    public void TestConstructor()
+    {
+        Sphere s = new Sphere();
+        Assert.True(Transformation.AreTransformationsClose(new Transformation(), s.Transform));
+    }
+
+    [Fact]
+    public void TestConstructorWithTransformation()
+    {
+        Transformation t = new Transformation(2, 7, 0.5f);
+        Sphere s = new Sphere(t);
+        Assert.Equal(t, s.Transform);
+    }
+
+    [Fact]
+    public void TestSphereNormal()
+    {
+        Sphere sphere = new Sphere();
+
+        Vector v = new Vector(1, 1, 1);
+        Point p1 = new Point(-1, 0, 0);
+        Assert.Equal(new Normal(-1, 0, 0), sphere._SphereNormal(p1, v));
+
+        Point p2 = new Point(0, 1, 1);
+        Assert.Equal(new Normal(0, -1, -1), sphere._SphereNormal(p2, v));
+    }
+
+    [Fact]
+    public void TestSpherePointToUV()
+    {
+        Sphere sphere = new Sphere();
+
+        Point p1 = new Point(0, 0, -1);
+        Assert.Equal(1, sphere._SpherePointToUV(p1).V);
+
+        Point p2 = new Point(0, 1, 0);
+        Assert.Equal(0.25f, sphere._SpherePointToUV(p2).U);
+    }
+
+    [Fact]
+    public void TestRayIntersection()
+    {
+        Ray ray = new Ray(new Point(0, 0, 2), new Vector(0, 0, -1));
+        Sphere sphere = new Sphere();
+        HitRecord? hit = sphere.RayIntersection(ray);
+        Assert.Equal(new Point(0, 0, 1), hit?.WorldPoint);
+        Assert.Equal(new Normal(0,0,1), hit?.SurfaceNormal);
+        
+        ray = new Ray(new Point(3, 0, 0), new Vector(-1, 0, 0));
+        hit = sphere.RayIntersection(ray);
+        Assert.Equal(new Point(1, 0, 0), hit?.WorldPoint);
+        Assert.Equal(new Normal(1,0,0), hit?.SurfaceNormal);
+        
+        ray = new Ray(new Point(0, 0, 0), new Vector(1, 0, 0));
+        hit = sphere.RayIntersection(ray);
+        Assert.Equal(new Point(1, 0, 0), hit?.WorldPoint);
+        Assert.Equal(new Normal(-1,0,0), hit?.SurfaceNormal);
+    }
+}
