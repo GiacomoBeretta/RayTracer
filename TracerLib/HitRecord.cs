82baebe732693fc1fa4b2cfb0afe04d393bd54eb
Giacomo Beretta <giacopot@gmail.com>
2026-04-26 02:22:09 +0200

    Implement HitRecord constructor and areClose function with tests

--- TracerLib/HitRecord.cs
+++ TracerLib/HitRecord.cs
@@ -4,5 +4,27 @@ namespace TracerLib;
 
 public struct HitRecord
 {
-    
+    public Point WorldPoint { get; }
+    public Normal SurfaceNormal { get; }
+    public Vector2D SurfacePoint { get; }
+    public Ray IncomingRay { get; }
+    public float T { get; }
+
+    public HitRecord(Point worldPoint, Normal normal, Vector2D surfacePoint, Ray ray, float t)
+    {
+        this.WorldPoint = worldPoint;
+        this.SurfaceNormal = normal;
+        this.SurfacePoint = surfacePoint;
+        this.IncomingRay = ray;
+        this.T = t;
+    }
+
+    public static bool _AreHitRecordsClose(HitRecord hit1, HitRecord hit2, float epsilon = 1e-5f)
+    {
+        return Point._ArePointsClose(hit1.WorldPoint, hit2.WorldPoint, epsilon)
+               && Normal._AreCloseNormal(hit1.SurfaceNormal, hit2.SurfaceNormal, epsilon)
+               && Vector2D._AreVectorsClose(hit1.SurfacePoint, hit2.SurfacePoint, epsilon)
+               && Ray._AreRaysClose(hit1.IncomingRay, hit2.IncomingRay,epsilon)
+               && Functions.AreClose(hit1.T, hit2.T,epsilon);
+    }
 }
