b6119528aeb3f6b3242ff651c7a765744180f81b
Simone Selmi <sselmi27@gmail.com>
2026-04-29 12:09:18 +0200

    First try generating black triangle

--- RayTracer/RayTracer.cs
+++ RayTracer/RayTracer.cs
@@ -1,18 +1,19 @@
 // This file is release under EUPL_v1.2 license. See LICENSE.md
 
+using SixLabors.ImageSharp.Processing;
 using TracerLib;
 
 //using TracerLib;
 public static class RayTracer
 {
-    public static string InputFileName = "";
+    /*public static string InputFileName = "";
     public static string OutputFileName = "";
     public static float AFactor = 1.0f;
-    public static float Gamma = 1.0f;
+    public static float Gamma = 1.0f;*/
 
-    public static int Main(string[] args)
+    public static int Main(/*string[] args*/)
     {
-        try
+        /*try
         {
             ParseArgs(args);
         }
@@ -20,7 +21,7 @@ public static class RayTracer
         {
             Console.WriteLine("Error: " + e.Message);
             return 1;
-        }
+        }*/
 
         string currentPath = AppDomain.CurrentDomain.BaseDirectory;
         string pfmFilePath = Path.Combine(currentPath, "../../../../TracerTests/reference_be.pfm");
@@ -30,6 +31,47 @@ public static class RayTracer
         hdrImage.Print();
         hdrImage.WritePNG(pngFilePathShirley, 0, AFactor, Gamma);
         hdrImage.WritePNG(pngFilePathWeighted, 1, AFactor, Gamma);*/
+
+        var image = new HDRImage(500, 500);
+
+        // PER LA CAMERA APPLICARE LA TRASLAZIONE PER PRIMA (ULTIMA NELLA CONCATENAZIONE)
+        var camera = new OrthogonalCamera(transformation:  new Transformation('y', MathF.PI/20) * new Transformation('z', MathF.PI/20) * new Transformation(new Vector(-1.0f, 0f, 0f)));
+
+        var tracer = new ImageTracer(image, camera);
+        
+        //PER LE SFERE APPLICARE LA TRASLAZIONE PER ULTIMA (PRIMA NELLA CONCATENAZIONE)
+        var s1 = new Sphere( new Transformation(new Vector(0.5f, 0.5f, 0.5f)) * new Transformation(0.1f, 0.1f, 0.1f));
+        var s2 = new Sphere( new Transformation(new Vector(-0.5f, 0.5f, 0.5f)) * new Transformation(0.1f, 0.1f, 0.1f));
+        var s3 = new Sphere( new Transformation(new Vector(0.5f, -0.5f, 0.5f)) * new Transformation(0.1f, 0.1f, 0.1f));
+        var s4 = new Sphere( new Transformation(new Vector(0.5f, 0.5f, -0.5f)) * new Transformation(0.1f, 0.1f, 0.1f));
+        var s5 = new Sphere( new Transformation(new Vector(-0.5f, -0.5f, 0.5f)) * new Transformation(0.1f, 0.1f, 0.1f));
+        var s6 = new Sphere( new Transformation(new Vector(0.5f, -0.5f, -0.5f)) * new Transformation(0.1f, 0.1f, 0.1f));
+        var s7 = new Sphere( new Transformation(new Vector(-0.5f, 0.5f, -0.5f)) * new Transformation(0.1f, 0.1f, 0.1f));
+        var s8 = new Sphere( new Transformation(new Vector(-0.5f, -0.5f, -0.5f)) * new Transformation(0.1f, 0.1f, 0.1f));
+        var s9 = new Sphere( new Transformation(new Vector(0f, 0f, -0.5f)) * new Transformation(0.1f, 0.1f, 0.1f));
+        var s10 = new Sphere( new Transformation(new Vector(0f, 0.5f, 0f)) * new Transformation(0.1f, 0.1f, 0.1f));
+        
+        //var p = new Plane(new Transformation(20f, 20f, 20f));
+        
+        var shapes = new List<Shape>
+        {
+            s1,
+            s2,
+            s3,
+            s4,
+            s5,
+            s6,
+            s7,
+            s8,
+            s9,
+            s10,
+        };
+
+        var world = new World(shapes);
+        
+        tracer.FireAllRays(ray => world.RayIntersection(ray) != null ? new Color(1.0f, 1.0f, 1.0f) : new Color(0.0f, 0.0f, 0.0f));
+        
+        image.WritePNG(pngFilePathShirley, 0,1.0f , 1.0f);
         
 
         /* suggerimento della lezione 4 in python
@@ -52,7 +94,7 @@ public static class RayTracer
         return 0;
     }
 
-    private static void ParseArgs(string[] args)
+    /*private static void ParseArgs(string[] args)
     {
         if (args.Length != 4)
         {
@@ -60,22 +102,22 @@ public static class RayTracer
                 "Usage: dotnet run inputFileName.pfm aFactor gamma outputFileName.png");
         }
 
-        InputFileName = args[0];
+        //InputFileName = args[0];
 
-        if (!Single.TryParse(args[1], out AFactor))
+        if (!Single.TryParse(args[0], out AFactor))
         {
             throw new ArgumentException($"Invalid aFactor ('{args[1]}'). It must be a floating number.",
                 nameof(AFactor));
         }
 
-        if (!Single.TryParse(args[2], out Gamma))
+        if (!Single.TryParse(args[1], out Gamma))
         {
             throw new ArgumentException($"Invalid gamma ('{args[2]}'). It must be a floating number.", nameof(Gamma));
         }
 
-        OutputFileName = args[3];
+        OutputFileName = args[2];
 
-        Console.WriteLine(
-            $"I parametri passati sono: InputFileName={args[0]}, aFactor={args[1]}, gamma={args[2]}, OutputFileName={args[3]}");
-    }
+        //Console.WriteLine(
+           // $"I parametri passati sono: InputFileName={args[0]}, aFactor={args[1]}, gamma={args[2]}, OutputFileName={args[3]}");
+    }*/
 }
