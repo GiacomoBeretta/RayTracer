7ac46a4ec693dfa0f40dfedb9584a19f2ddcaaf3
Giacomo Beretta <giacopot@gmail.com>
2026-05-18 16:15:19 +0200

    add docstrings

--- TracerLib/PCG.cs
+++ TracerLib/PCG.cs
@@ -4,7 +4,12 @@ public class PCG
 {
     public ulong State { get; set; }
     public ulong Inc { get; set; }
-
+    
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="initState"></param>
+    /// <param name="initSeq"></param>
     public PCG(ulong initState = 42, ulong initSeq = 54)
     {
         State = 0;
@@ -14,6 +19,10 @@ public class PCG
         this.Random();
     }
 
+    /// <summary>
+    /// Returns a random unsigned integer (32 bit) number between 0 and uint.max
+    /// </summary>
+    /// <returns></returns>
     public uint Random()
     {
         var oldstate = State;
@@ -27,6 +36,10 @@ public class PCG
         return (xorshifted >> rot) | (xorshifted << ((~rot + 1) & 31)); // Rivedere con 32 - rot al posto di ~rot +1
     }
 
+    /// <summary>
+    /// Returns a random floating number between 0 and 1
+    /// </summary>
+    /// <returns></returns>
     public float RandomFloat()
     {
         return this.Random() / 0x100000000;
