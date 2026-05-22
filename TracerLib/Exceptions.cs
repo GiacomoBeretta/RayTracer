530684abbaa12c75f96b8a34a99c72642196e466
Giacomo Beretta <giacopot@gmail.com>
2026-05-22 19:43:20 +0200

    Add GrammarError exception

--- TracerLib/Exceptions.cs
+++ TracerLib/Exceptions.cs
@@ -12,6 +12,24 @@ public class InvalidPfmFileFormat : FormatException
     public InvalidPfmFileFormat(string message, Exception inner) : base(message, inner) { }
 }
 
+//e come si stampa poi la location?
+public class GrammarError : Exception
+{
+    private SourceLocation location;
+    
+    public GrammarError(string message) : base(message) { }
+    public GrammarError(string message, Exception inner) : base(message, inner) { }
+    public GrammarError(string message, SourceLocation location) : base(message)
+    {
+        this.location = location;
+    }
+    public GrammarError(string message, SourceLocation location, Exception inner) : base(message, inner)
+    {
+        this.location = location;
+    }
+    
+}
+
 /* public class ZeroDivision : ArithmeticException
 {
     public ZeroDivision(string error) : base(error){}
