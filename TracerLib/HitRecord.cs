569a170f7f36ffe06523cb2ed7bfc1c86093487a
Giacomo Beretta <giacopot@gmail.com>
2026-06-09 19:13:35 +0200

    Change name from SurfacePoint to SurfacePosition (HitRecord). Add docstrings

--- TracerLib/HitRecord.cs
+++ TracerLib/HitRecord.cs
@@ -4,23 +4,38 @@ namespace TracerLib;
 
 /// <summary>
 /// A struct representing the intersection between a shape and a ray.
-/// 
-/// WorldPoint is the point where the intersection occurs.
-/// SurfaceNormal is the surface normal at the intersection point, typically oriented
-/// against the incoming ray direction.
-/// T is the parameter such that WorldPoint = IncomingRay.Origin + T * IncomingRay.Dir
 /// </summary>
 public struct HitRecord
 {
-    public Point WorldPoint { get; }
-    public Shape Shape { get; }/// <summary>
-    /// Converts degrees to radians using the formula: radians = degrees * π / 180.
+    /// <summary>
+    /// The point where the intersection occurs.
     /// </summary>
-    /// <param name="deg">Angle in degrees.</param>
-    /// <returns>Angle in radians.</returns>
+    public Point WorldPoint { get; }
+
+    /// <summary>
+    /// The shape intersected by the IncomingRay.
+    /// </summary>
+    public Shape Shape { get; }
+
+    /// <summary>
+    /// SurfaceNormal is the surface normal at the intersection point,
+    /// typically oriented against the incoming ray direction.
+    /// </summary>
     public Normal SurfaceNormal { get; }
-    public Vector2D SurfacePoint { get; }
+
+    /// <summary>
+    /// The 2D surface coordinates of the intersection point WorldPoint.
+    /// </summary>
+    public Vector2D SurfacePosition { get; }
+
+    /// <summary>
+    /// The ray that intersects the shape.
+    /// </summary>
     public Ray IncomingRay { get; }
+
+    /// <summary>
+    /// T is the parameter such that WorldPoint = IncomingRay.Origin + T * IncomingRay.Dir
+    /// </summary>
     public float T { get; }
 
     public HitRecord(Point worldPoint, Shape shape, Normal normal, Vector2D surfacePoint, Ray ray, float t)
@@ -28,7 +43,7 @@ public struct HitRecord
         this.WorldPoint = worldPoint;
         this.Shape = shape;
         this.SurfaceNormal = normal;
-        this.SurfacePoint = surfacePoint;
+        this.SurfacePosition = surfacePoint;
         this.IncomingRay = ray;
         this.T = t;
     }
@@ -38,7 +53,7 @@ public struct HitRecord
         return Point._ArePointsClose(hit1.WorldPoint, hit2.WorldPoint, epsilon)
                && (hit1.Shape)._IsCloseTo(hit2.Shape, epsilon)
                && Normal._AreNormalsClose(hit1.SurfaceNormal, hit2.SurfaceNormal, epsilon)
-               && Vector2D._AreVectorsClose(hit1.SurfacePoint, hit2.SurfacePoint, epsilon)
+               && Vector2D._AreVectorsClose(hit1.SurfacePosition, hit2.SurfacePosition, epsilon)
                && Ray._AreRaysClose(hit1.IncomingRay, hit2.IncomingRay, epsilon)
                && Functions.AreClose(hit1.T, hit2.T, epsilon);
     }
@@ -48,7 +63,7 @@ public struct HitRecord
         return "HitRecord(WorldPoint=" + WorldPoint +
                ", Shape=" + Shape +
                ", SurfaceNormal=" + SurfaceNormal +
-               ", SurfacePoint=" + SurfacePoint +
+               ", SurfacePoint=" + SurfacePosition +
                ", IncomingRay=" + IncomingRay +
                ", T=" + T;
     }
