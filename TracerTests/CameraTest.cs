eb69aac09338fcfab3ad465c0290206bf05483b1
Giacomo Beretta <giacopot@gmail.com>
2026-06-11 23:17:59 +0200

    change the coordinates for the tests of FireRay for the orthogonal and perspective camera and the tests of uv_mapping and uv_submapping, after the change of the origin for the u,v screen coordinates and the origin of the uPixel, vPixel coordinates inside the pixel. Added some more tests

--- TracerTests/CameraTest.cs
+++ TracerTests/CameraTest.cs
@@ -7,53 +7,67 @@ public class CameraTest
     [Fact]
     public void FireRayOrthogonalTest()
     {
-        OrthogonalCamera cam = new OrthogonalCamera(new Transformation(), 2.0f); //Cercare metodo default
+        OrthogonalCamera cam = new OrthogonalCamera(new Transformation(), 2.0f);
 
         Ray ray1 = cam.FireRay(0.0f, 0.0f);
         Ray ray2 = cam.FireRay(1.0f, 0.0f);
         Ray ray3 = cam.FireRay(0.0f, 1.0f);
         Ray ray4 = cam.FireRay(1.0f, 1.0f);
+        Ray ray5 = cam.FireRay(0.5f, 0.5f);
+
+        //Verify that the rays are parallel to the ray pointing towards the x-axis.
+        Vector x_vector = new Vector(1, 0, 0);
+        Assert.True(Vector._AreVectorsClose(x_vector, ray1.Dir));
+        Assert.True(Vector._AreVectorsClose(x_vector, ray2.Dir));
+        Assert.True(Vector._AreVectorsClose(x_vector, ray3.Dir));
+        Assert.True(Vector._AreVectorsClose(x_vector, ray4.Dir));
+        Assert.True(Vector._AreVectorsClose(x_vector, ray5.Dir));
         
-        //Verify that two rays are parallel by vanishing the cross product
+        //Verify that the rays are parallel between each other by vanishing the cross product
         Assert.True(Functions.AreClose(0.0f, Vector.CrossProduct(ray1.Dir, ray2.Dir).SquaredNorm()));
         Assert.True(Functions.AreClose(0.0f, Vector.CrossProduct(ray1.Dir, ray3.Dir).SquaredNorm()));
         Assert.True(Functions.AreClose(0.0f, Vector.CrossProduct(ray1.Dir, ray4.Dir).SquaredNorm()));
-        
-        Assert.True(Point._ArePointsClose(ray1.At(1.0f), new Point(0.0f, 2.0f, -1.0f)));
-        Assert.True(Point._ArePointsClose(ray2.At(1.0f), new Point(0.0f, -2.0f, -1.0f)));
-        Assert.True(Point._ArePointsClose(ray3.At(1.0f), new Point(0.0f, 2.0f, 1.0f)));
-        Assert.True(Point._ArePointsClose(ray4.At(1.0f), new Point(0.0f, -2.0f, 1.0f)));
+        Assert.True(Functions.AreClose(0.0f, Vector.CrossProduct(ray1.Dir, ray5.Dir).SquaredNorm()));
+
+        Assert.True(Point._ArePointsClose(new Point(0.0f, 2f, 1f), ray1.At(1.0f)));
+        Assert.True(Point._ArePointsClose(new Point(0.0f, -2f, 1f), ray2.At(1.0f)));
+        Assert.True(Point._ArePointsClose(new Point(0.0f, 2f, -1f), ray3.At(1.0f)));
+        Assert.True(Point._ArePointsClose(new Point(0.0f, -2f, -1f), ray4.At(1.0f)));
+        Assert.True(Point._ArePointsClose(new Point(0.0f, 0, 0), ray5.At(1.0f)));
     }
 
     [Fact]
     public void FireRayPerspectiveTest()
     {
         PerspectiveCamera cam = new PerspectiveCamera(new Transformation(), 1f, 2f);
-        
+
         Ray ray1 = cam.FireRay(0.0f, 0.0f);
         Ray ray2 = cam.FireRay(1.0f, 0.0f);
         Ray ray3 = cam.FireRay(0.0f, 1.0f);
         Ray ray4 = cam.FireRay(1.0f, 1.0f);
-        
-        Assert.True(Point._ArePointsClose(ray1.Origin, ray2.Origin));
-        Assert.True(Point._ArePointsClose(ray1.Origin, ray3.Origin));
-        Assert.True(Point._ArePointsClose(ray1.Origin, ray4.Origin));
-        
-        ray1.At(1.0f).Print();
-        
-        Assert.True(Point._ArePointsClose(ray1.At(1.0f), new Point(0.0f, 2.0f, -1.0f)));
-        Assert.True(Point._ArePointsClose(ray2.At(1.0f), new Point(0.0f, -2.0f, -1.0f)));
-        Assert.True(Point._ArePointsClose(ray3.At(1.0f), new Point(0.0f, 2.0f, 1.0f)));
-        Assert.True(Point._ArePointsClose(ray4.At(1.0f), new Point(0.0f, -2.0f, 1.0f)));
+        Ray ray5 = cam.FireRay(0.5f, 0.5f);
+
+        Point origin = new Point(-1, 0, 0);
+        Assert.True(Point._ArePointsClose(origin, ray1.Origin));
+        Assert.True(Point._ArePointsClose(origin, ray2.Origin));
+        Assert.True(Point._ArePointsClose(origin, ray3.Origin));
+        Assert.True(Point._ArePointsClose(origin, ray4.Origin));
+        Assert.True(Point._ArePointsClose(origin, ray5.Origin));
+
+        Assert.True(Point._ArePointsClose(new Point(0.0f, 2f, 1f), ray1.At(1.0f)));
+        Assert.True(Point._ArePointsClose(new Point(0.0f, -2f, 1f), ray2.At(1.0f)));
+        Assert.True(Point._ArePointsClose(new Point(0.0f, 2f, -1f), ray3.At(1.0f)));
+        Assert.True(Point._ArePointsClose(new Point(0.0f, -2f, -1f), ray4.At(1.0f)));
+        Assert.True(Point._ArePointsClose(new Point(0.0f, 0, 0), ray5.At(1.0f)));
     }
 
     [Fact]
     public void OrthogonalTransformTest()
     {
         OrthogonalCamera cam = new OrthogonalCamera(transformation: new Transformation(new Vector(0.0f, -2.0f, 0.0f)) *
-                                                       new Transformation('z', MathF.PI / 2));
+                                                                    new Transformation('z', MathF.PI / 2));
         Ray ray = cam.FireRay(0.5f, 0.5f);
-        
+
         Assert.True(Point._ArePointsClose(ray.At(1.0f), new Point(0.0f, -2.0f, 0.0f)));
     }
 
@@ -61,9 +75,9 @@ public class CameraTest
     public void PerspectiveTransformTest()
     {
         OrthogonalCamera cam = new OrthogonalCamera(transformation: new Transformation(new Vector(0.0f, -2.0f, 0.0f)) *
-                                                       new Transformation('z', MathF.PI / 2));
+                                                                    new Transformation('z', MathF.PI / 2));
         Ray ray = cam.FireRay(0.5f, 0.5f);
-        
+
         Assert.True(Point._ArePointsClose(ray.At(1.0f), new Point(0.0f, -2.0f, 0.0f)));
     }
 }
--- TracerTests/ImageTracerTest.cs
+++ TracerTests/ImageTracerTest.cs
@@ -23,35 +23,52 @@ public class ImageTracerTest
     {
         //ImageTracerTest test = new ImageTracerTest();
 
-        Ray ray1 = tracer.FireRayAtPixel(0, 1, 2.5f, 1.5f);
-        Ray ray2 = tracer.FireRayAtPixel(2, 0, 0.5f, 0.5f);
-        Assert.True(Ray._AreRaysClose(ray1, ray2));
+        // pixel at top left corner, and top left corner of the pixel
+        Ray ray1 = tracer.FireRayAtPixel(0, 0, 0f, 0f); 
+        Assert.True(Point._ArePointsClose(new Point(0f, 2f, 1f), ray1.At(1.0f)));
 
-        ray1 = tracer.FireRayAtPixel(0, 0, 0, 1);
-        ray2 = tracer.FireRayAtPixel(0, 1, 0, 2);
-        Assert.True(Ray._AreRaysClose(ray1, ray2));
+        // top right corner of the pixel
+        ray1 = tracer.FireRayAtPixel(0, 0, 1f, 0f);
+        Assert.True(Point._ArePointsClose(new Point(0f, 1f, 1f), ray1.At(1.0f)));
+        
+        // bottom left corner of the pixel
+        ray1 = tracer.FireRayAtPixel(0, 0, 0f, 1f);
+        Assert.True(Point._ArePointsClose(new Point(0f, 2f, 0f), ray1.At(1.0f)));
+        
+        // bottom right corner of the pixel
+        ray1 = tracer.FireRayAtPixel(0, 0, 1f, 1f);
+        Assert.True(Point._ArePointsClose(new Point(0f, 1f, 0f), ray1.At(1.0f)));
+        
+        // We assign unusual values for uPixel and vPixel although these values are never reached 
+        Ray ray2 = tracer.FireRayAtPixel(0, 0, 2.5f, 1.5f);
+        Ray ray3 = tracer.FireRayAtPixel(2, 1, 0.5f, 0.5f);
+        Assert.True(Ray._AreRaysClose(ray2, ray3));
 
-        ray1 = tracer.FireRayAtPixel(3, 0, 1, 1);
-        ray2 = tracer.FireRayAtPixel(0, 1, 4, 2);
-        Assert.True(Ray._AreRaysClose(ray1, ray2));
+        ray2 = tracer.FireRayAtPixel(0, 0, 0, 1);
+        ray3 = tracer.FireRayAtPixel(0, 1, 0, 0);
+        Assert.True(Ray._AreRaysClose(ray2, ray3));
+
+        ray2 = tracer.FireRayAtPixel(3, 0, 1, 1);
+        ray3 = tracer.FireRayAtPixel(0, 1, 4, 0);
+        Assert.True(Ray._AreRaysClose(ray2, ray3));
     }
 
     [Fact]
     public void Test_uv_mapping()
     {
-        Ray ray = tracer.FireRayAtPixel(0, 0, 0, 1);
-        Point expectedPoint = new Point(0, camera.AspectRatio, 1); // top left of the screen
+        Ray ray = tracer.FireRayAtPixel(0, 0, 0, 0);
+        Point expectedPoint = new Point(0, camera.AspectRatio, 1f); // top left of the screen
         Assert.True(Point._ArePointsClose(expectedPoint, ray.At(1)));
 
-        ray = tracer.FireRayAtPixel(3, 0, 1, 1);
+        ray = tracer.FireRayAtPixel(3, 0, 1, 0);
         expectedPoint = new Point(0, -camera.AspectRatio, 1); // top right of the screen
         Assert.True(Point._ArePointsClose(expectedPoint, ray.At(1)));
 
-        ray = tracer.FireRayAtPixel(0, 1, 0, 0);
+        ray = tracer.FireRayAtPixel(0, 1, 0, 1);
         expectedPoint = new Point(0, camera.AspectRatio, -1); // bottom left of the screen
         Assert.True(Point._ArePointsClose(expectedPoint, ray.At(1)));
 
-        ray = tracer.FireRayAtPixel(3, 1, 1, 0);
+        ray = tracer.FireRayAtPixel(3, 1, 1, 1);
         expectedPoint = new Point(0, -camera.AspectRatio, -1); // bottom right of the screen
         Assert.True(Point._ArePointsClose(expectedPoint, ray.At(1)));
     }
