d9ca698f1d921b5dde2d1cc44e2df90c3d53cd8d
Giacomo Beretta <giacopot@gmail.com>
2026-06-25 17:00:27 +0200

    change name of VariableTable to ParseVariableTable and change in initialization of scene object after the return type change of ParseScene

--- RayTracer/RayTracer.cs
+++ RayTracer/RayTracer.cs
@@ -486,11 +486,11 @@ public class RenderCommand
 
         var scene = new Scene();
         var input = new InputStream(scenePath);
-        var variables = Functions.VariableTable(Definitions);
+        var variables = Functions.ParseVariableTable(Definitions);
 
         try
         {
-            scene = scene.ParseScene(input, variables);
+            scene.ParseScene(input, variables);
         }
         catch (Exception e)
         {
