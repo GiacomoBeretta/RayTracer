caf97309aa3e045184173975938b998c105c980a
Giacomo Beretta <giacopot@gmail.com>
2026-06-26 17:41:39 +0200

    change config parameter names: lumfunction -> luminosityfunction, inputrender -> inputscene (render command), output -> outputpng (pfmtopng command), outputaveragepfm -> outputpfm, outputaveragepng -> outputpng (average command)

--- config.sh
+++ config.sh
@@ -1,33 +1,32 @@
 # Global default 
-lumfunction="shirley"
+luminosityfunction="shirley"
 averageluminosity=0,5
 factor=1
 gamma=1
 
 # Default render
-inputrender="scene.txt"
-width=500
-height=500
-algorithm="flat"
+inputscene="scene.txt"
 outputpfm="scene_test.pfm"
 outputpng="scene_test.png"
+width=500
+height=500
+sampleside=1
+algorithm="flat"
 numrays=10
 maxdepth=2
 initstate=(45)
 initseq=(54)
-sampleside=1
-declarefloat=()
 roulettestart=3
 rouletteprob=""
+declarefloat=()
 
 # Default pfmtopng
 inputpfm="dome.pfm"
-output="dome.png"
+outputpng="dome.png"
 
-# Default averageimage
-outputaveragepfm="average.pfm"
-outputaveragepng="average.png"
+# Default averageimages
+outputpfm="average.pfm"
+outputpng="average.png"
 
 # Cycle parameters
 pcgcycle=false
-
