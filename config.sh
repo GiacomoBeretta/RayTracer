fa446d0ffa7cc610044e33405fd12b3d3c3f2039
Giacomo Beretta <giacopot@gmail.com>
2026-06-29 15:27:16 +0200

    Change config values

--- config.sh
+++ config.sh
@@ -1,8 +1,8 @@
 # Parameters used if not specified during the execution of raytracer.sh
 
 # Default Common options
-outputpfm="scene_test.pfm" #not used by pfmtopng
-outputpng="prova.png"
+outputpfm="demo.pfm" #not used by pfmtopng
+outputpng="demo.png"
 luminosityfunction="shirley"
 averageluminosity=""
 factor=1
@@ -10,19 +10,19 @@ gamma=1
 
 # Default render 
 inputscene="scene.txt"
-width=100
-height=100
+width=500
+height=500
 sampleside=1
 algorithm="pathtracing"
-numrays=5 #only for pathtracing
-maxdepth=4 #only for pathtracing
-initstate=(45) #only for pathtracing
-initseq=(54) #only for pathtracing
+numrays=10 #only for pathtracing
+maxdepth=2 #only for pathtracing
+initstate=(45 22) #only for pathtracing
+initseq=(54 13) #only for pathtracing
 roulettestart=3 #only for pathtracing
 rouletteprob=0.5 #only for pathtracing
 declarefloat=()
 
-pcgcycle=false #only for pathtracing
+pcgcycle=true #only for pathtracing
 
 # Default pfmtopng
 inputpfm="dome.pfm"
