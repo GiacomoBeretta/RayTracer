13468a1c43cba1edcf3bb65a8aef8829e8b5ab65
Giacomo Beretta <giacopot@gmail.com>
2026-06-29 09:38:12 +0200

    Reorder config.sh

--- config.sh
+++ config.sh
@@ -1,29 +1,29 @@
 # Parameters used if not specified during the execution of raytracer.sh
 
-# Global default
+# Default Common options
 outputpfm="scene_test.pfm" #not used by pfmtopng
-outputpng="scene_test.png"
+outputpng="prova.png"
 luminosityfunction="shirley"
 averageluminosity=""
 factor=1
 gamma=1
 
-# Default render
+# Default render 
 inputscene="scene.txt"
 width=500
 height=500
 sampleside=1
 algorithm="flat"
-numrays=10
-maxdepth=2
-initstate=(45)
-initseq=(54)
-roulettestart=3
-rouletteprob=""
+numrays=10 #only for pathtracing
+maxdepth=2 #only for pathtracing
+initstate=(45) #only for pathtracing
+initseq=(54) #only for pathtracing
+roulettestart=3 #only for pathtracing
+rouletteprob="" #only for pathtracing
 declarefloat=()
+
+pcgcycle=false #only for pathtracing
 
 # Default pfmtopng
 inputpfm="dome.pfm"
 
-# Cycle parameters
-pcgcycle=false
